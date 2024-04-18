0d4a9468be16c1ccd5d2ff9dfd5210652d2b2838
Алексей Комков <ale45883412@yandex.ru>
2024-04-19 03:27:19 +0700

    google_api

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,6 @@
-from .investmentService import investmentService # noqa
+from .investment_service import investmentService # noqa
+from .google_api import (
+    spreadsheets_create,
+    set_user_permissions,
+    spreadsheets_update_value,
+) # noqa
