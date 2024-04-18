c408178eae1f09740a7e4665171b4ec9e79a69d9
Алексей Комков <ale45883412@yandex.ru>
2024-04-18 21:59:19 +0700

    Дополнил класс Settings новыми переменными

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,18 @@ class Settings(BaseSettings):
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
+    # Переменные для Google API
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         env_file = '.env'
