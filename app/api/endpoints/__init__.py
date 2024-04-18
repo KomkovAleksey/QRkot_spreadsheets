4e3dc57eaef72ae88311a1487c49face44e877c9
Алексей Комков <ale45883412@yandex.ru>
2024-04-19 03:23:16 +0700

    google_api_router

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,3 +1,4 @@
 from .charity_project import router as charity_project_router  # noqa
 from .donation import router as donation_router  # noqa
 from .user import router as user_router  # noqa
+from .google_api import router as google_api_router  # noqa
