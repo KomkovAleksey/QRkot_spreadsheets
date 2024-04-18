dc81e8f5659b77a4b5e6a08e5cdd35dc5b2c7965
Алексей Комков <ale45883412@yandex.ru>
2024-04-19 03:12:01 +0700

    google_api_router

--- app/api/routers.py
+++ app/api/routers.py
@@ -7,6 +7,7 @@ from app.api.endpoints import (
     charity_project_router,
     donation_router,
     user_router,
+    google_api_router,
 )
 
 main_router = APIRouter()
@@ -15,5 +16,8 @@ main_router.include_router(
 )
 main_router.include_router(
     donation_router, prefix='/donation', tags=['Donation']
+)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
 )
 main_router.include_router(user_router)
